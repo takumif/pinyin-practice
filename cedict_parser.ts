837bf53cc9b6d3e2d9d7f16273fe1807a79da37f
Takumi Fujimoto <takumi@takumium.com>
2016-03-24 00:58:06 -0400

    Add cedict_parser, cedict_tree, entry, and tsconfig

---
+++ cedict_parser.ts
@@ -0,0 +1,49 @@
+/// <reference path="entry.ts" />
+
+class CedictParser {
+    /**
+     * Parses a CEDICT text file into a list of entries
+     */
+    static parse(url: string): Entry[] {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.open("GET", url, false); // open synchronously
+        xhr.send();
+        return CedictParser.parseCedictText(xhr.responseText);
+    }
+    
+    private static parseCedictText(text: string): Entry[] {
+        var lines = text.split("\n");
+        var entries: Entry[] = [];
+        
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            
+            // ignore non-entry lines
+            if (line.length === 0 || line[0] === "#") {
+                continue;
+            }
+            entries.push(CedictParser.parseCedictLine(line));    
+        }
+        return entries;
+    }
+    
+    private static parseCedictLine(line: string): Entry {
+        // Entries have this format:
+        // TRADITIONAL SIMPLIFIED [PINYIN] /ENGLISH 1/ENGLISH 2/
+        
+        var firstSpace = line.indexOf(" ");
+        var secondSpace = line.indexOf(" ", firstSpace + 1);
+        var leftBracket = line.indexOf("[");
+        var rightBracket = line.indexOf("]");
+        var firstSlash = line.indexOf("/");
+        var lastChar = line.length - 1;
+        
+        var traditional = line.substr(0, firstSpace);
+        var simplified = line.substr(firstSpace + 1, secondSpace - firstSpace - 1);
+        var pinyin = line.substr(leftBracket + 1, rightBracket - leftBracket - 1);
+        var english = line.substr(firstSlash + 1, lastChar - firstSlash - 1);
+        
+        return new Entry(traditional, simplified, pinyin, english);
+    }
+}
